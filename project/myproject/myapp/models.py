10f01236d5cdd130c3c7ab3d80881de173773ee8
jeyhun <git-ceyhun071173@mail.ru>
2024-01-12 18:00:15 +0400

    Zadaniye _3

--- project/myproject/myapp/models.py
+++ project/myproject/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Client(models.Model):
@@ -23,11 +24,33 @@ class Product(models.Model):
     def __str__(self):
         return f'Product: {self.name}, Price: {self.price}, Quantity: {self.quantity}'
 
+# class Order(models.Model):
+#     client = models.ForeignKey(Client, on_delete=models.CASCADE)
+#     products = models.ManyToManyField(Product)
+#     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+#     order_date = models.DateField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f'Order #{self.id} by {self.client.name}, Total Amount: {self.total_amount}, Order Date: {self.order_date}'
+    
+
+
+# class OrderManager(models.Model):
+    
+#     @staticmethod
+#     def get_products_in_time_range(orders, start_date):
+#         return Product.objects.filter(order__in=orders.filter(order_date__gte=start_date)).distinct()
+
+# class Order(models.Model):
+#     objects = OrderManager()
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 class Order(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    order_date = models.DateField(auto_now_add=True)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
 
     def __str__(self):
-        return f'Order #{self.id} by {self.client.name}, Total Amount: {self.total_amount}, Order Date: {self.order_date}'
+       return f'Order #{self.id} by {self.client.name}, Total Amount: {self.total_amount}, Order Date: {self.order_date}'
